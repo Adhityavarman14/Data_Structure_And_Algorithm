8106f35377e343d5c5a5f005768b3da190b2836c
Adhityavarman S S <113232708+Adhityavarman14@users.noreply.github.com>
2023-07-21 12:10:23 +0530

    Create Count_of_OddOrEven.java

---
+++ day8/Count_of_OddOrEven.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class AA {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		char arr[] = String.valueOf(a).toCharArray();
+		int len = arr.length,even = 0, odd = 0;
+		char even1[] = new char[len],odd1[] = new char[len];int j = 0,k = 0;
+		
+		for(int i = 0;i< len;i++) {
+			if(arr[i]%2 == 0) {
+				even1[j]=arr[i];
+				j++;
+				even++;
+				continue;
+		}
+			odd1[k]=arr[i];
+			k++;
+			odd++;
+		}
+		System.out.println(even+" "+odd);
+		for(int i = 0;i < even1.length;i++) {
+			System.out.println("even "+"odd");
+			System.out.println(" "+even1[i]+"     "+odd1[i]);
+		}
+	}
+}
